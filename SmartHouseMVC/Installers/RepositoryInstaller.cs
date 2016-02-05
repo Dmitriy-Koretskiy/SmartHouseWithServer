979b0894e0d7168a4995afde2b4cc808dd0dee00
Dmitriy-Koretskiy <dmitriy.koretskiy@gmail.com>
2016-02-05 14:28:47 +0200

    Add IoC installers

---
+++ SmartHouseMVC/Installers/RepositoryInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using DAL;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouseWebSite.Installers
+{
+    public class RepositoryInstaller: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IRepository>().ImplementedBy<Repository>().LifestyleSingleton(),
+                Component.For<DbContext>().ImplementedBy<SmartHouseContext>().LifestyleSingleton());
+        }
+    }
+}
